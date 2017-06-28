070f74552bfffb0066103370cd7893977f7dfbbc
Nivedha15 <nivinivisindhu96@gmail.com>
2017-06-28 16:34:24 +0530

    Create rangeprime.c

---
+++ rangeprime.c
@@ -0,0 +1,27 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int n,m,i,j,f=0;
+
+scanf("%d%d",&n,&m);
+for(i=n+1;i<m;i++)
+{
+for(j=2;j<n/2;j++)
+{
+if(i%j==0)
+{
+f=1;
+break;
+}
+}
+if(f==0)
+{
+printf("%d,",i);
+}
+f=0;
+}
+getch();
+}
+
+
